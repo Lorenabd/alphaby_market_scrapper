1f7c9f00d1256cbfeea295c3e7ece77b5575fb9c
osboxes.org <lorena.burgosd@alumnos.upm.es>
2025-09-13 07:50:47 -0400

    interface changed

--- pop_up_continue.py
+++ pop_up_continue.py
@@ -1,34 +1,40 @@
-from PyQt5.QtWidgets import QApplication,QDialogButtonBox,QDialog, QWidget, QPushButton, QLabel, QVBoxLayout,QLineEdit
+from PyQt5.QtWidgets import (
+    QApplication,
+    QDialogButtonBox,
+    QDialog,
+    QWidget,
+    QPushButton,
+    QLabel,
+    QVBoxLayout,
+    QLineEdit,
+)
 import sys
+
 
 class WindowContinue(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.input_field = QLineEdit() 
+        self.input_field = QLineEdit()
         self.pop_up_info()
-        
-    def pop_up_info(self):
-        # app = QApplication(sys.argv)
-        #window = QWidget()
-        self.setWindowTitle('Scrapper Information!')
-        
 
-        message = QLabel("If you want to continue extracting data\n go to the category and press continue.\n If not press Exit to finish extraction")
+    def pop_up_info(self):
+        self.setWindowTitle("Important Information!")
+
         input_label = QLabel("Enter the name of the output file:")
-        
-        buttons = QDialogButtonBox(
-            QDialogButtonBox.Ok | QDialogButtonBox.Cancel,
-            self
+        message = QLabel(
+            "If you want to continue extracting data\ngo to the category and press continue.\nIf not, press Exit to finish extraction"
         )
+
+        buttons = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, self)
         buttons.button(QDialogButtonBox.Ok).setText("Continue")
         buttons.button(QDialogButtonBox.Cancel).setText("Exit")
         buttons.accepted.connect(self.accept)
         buttons.rejected.connect(self.reject)
 
         layout = QVBoxLayout(self)
-        layout.addWidget(message)
         layout.addWidget(input_label)
         layout.addWidget(self.input_field)
+        layout.addWidget(message)
         layout.addWidget(buttons)
 
         self.setLayout(layout)
